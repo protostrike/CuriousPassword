a81db6929d884be95c4d35881edaa80fd93f5d73
protostrike <alexmeng1014@gmail.com>
2018-03-27 17:29:26 -0400

    prototype update
    Add GUI to both programs.
    Can be used to test for one password now.
    TODO: error checking for user input.

--- src/PasswordGetter.java
+++ src/PasswordGetter.java
@@ -1,13 +1,19 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
 public class PasswordGetter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(getPassword("138008103010", readBooklet()));
+		createSwing();
 	}
 	//read from text file and store every line into a list
 		private static List<String[]> readBooklet(){
@@ -22,7 +28,9 @@ public class PasswordGetter {
 			}
 			return lis;
 		}
-		private static String getPassword(String password, List<String[]> booklet) {
+		private static String getPassword(String password) {
+			//TODO: Error checking
+			List<String[]> booklet = readBooklet();
 			String[] splitedPassword = new String[4];
 			String literalPassword = "";
 			splitedPassword = password.split("(?<=\\G...)");
@@ -32,5 +40,27 @@ public class PasswordGetter {
 				literalPassword += booklet.get(row)[word];
 			}
 			return literalPassword;
+		}
+		private static void createSwing() {
+			JFrame main = new JFrame();
+			JTextArea password = new JTextArea();
+			JTextField number = new JTextField();
+			JButton b = new JButton("Get");
+			b.setBounds(100, 100, 150, 30);
+			b.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					password.setText(getPassword(number.getText()));
+				}
+			});
+			password.setBounds(100, 10, 150, 30);
+			number.setBounds(100, 150, 150, 30);
+			main.add(b);
+			main.add(number);
+			main.add(password);
+			main.setSize(800,600);
+			main.setResizable(false);
+			main.setTitle("Password Getter");
+			main.setVisible(true);
 		}
 }
--- src/passwordGenerator.java
+++ src/passwordGenerator.java
@@ -1,6 +1,9 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
+import javax.swing.*;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,9 +11,7 @@ import java.util.List;
 public class passwordGenerator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		List<String[]> passwordList = readBooklet();
-		System.out.println(generatePassword(passwordList));
+		createSwing();
 	}
 	//read from text file and store every line into a list
 	private static List<String[]> readBooklet(){
@@ -25,15 +26,17 @@ public class passwordGenerator {
 		}
 		return lis;
 	}
-	
-	private static String generatePassword(List<String[]> passwordList) {
+	//Generate encrypted password and store actual password
+	private static String generatePassword(String actualPassword) {
+		List<String[]> passwordList = readBooklet();
 		int randomRow, randomWord;
 		String password = "";
 		for(int i = 0; i<2; i++) {
-			randomRow = (int) (Math.random()*passwordList.size()+1);
-			randomWord = (int) (Math.random()*(passwordList.get(randomRow).length)+1);
+			randomRow = (int) (Math.random()*(passwordList.size()+1));
+			randomWord = (int) (Math.random()*((passwordList.get(randomRow).length)+1));
 			password += intFormat(randomRow);
 			password += intFormat(randomWord);
+			actualPassword += passwordList.get(randomRow)[randomWord];
 		}
 		return password;
 	}
@@ -45,5 +48,59 @@ public class passwordGenerator {
 			return "0"+integer;
 		else
 			return Integer.toString(integer);
+	}
+	
+	private static void createSwing() {
+		String bankPassword = "";
+		JFrame main = new JFrame();
+		JLabel bankLabel = new JLabel("Your banking password");
+		bankLabel.setBounds(10, 10, 150, 30);
+		JButton b = createPasswordButton(bankPassword);
+		b.setBounds(20, 40, 100, 30);
+		JTextField vali = new JTextField();
+		vali.setBounds(20, 80, 100, 30);
+		JButton v = createValidateButton(bankPassword, vali.getText());
+		v.setBounds(20, 120, 100, 30);
+		JPanel newPanel = new JPanel();
+		newPanel.add(bankLabel);
+		newPanel.setLayout(new BorderLayout());
+		newPanel.setBounds(0, 0, 150, 450);
+		main.add(b);
+		main.add(v);
+		main.add(vali);
+		main.add(newPanel);
+		main.setSize(800, 600);
+		main.setLayout(null);
+		main.setResizable(false);
+		main.setTitle("Password Generator");
+		main.setVisible(true);
+	}
+	//create button with eventListener
+	//Note: you need a textArea to link the listener
+	protected static JButton createPasswordButton(String actualPassword) {
+		JButton button = new JButton("Get");
+		button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				JOptionPane.showMessageDialog(null, "your password is "+generatePassword(actualPassword), "pPassword", JOptionPane.PLAIN_MESSAGE);
+			}
+		});
+		return button;
+	}
+	protected static JButton createValidateButton(String password, String testPassword) {
+		JButton button = new JButton("Validate");
+		button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				JOptionPane.showMessageDialog(null, "your password is "+testPassword(password, testPassword), "Validate", JOptionPane.PLAIN_MESSAGE);
+			}
+		});
+		return button;
+	}
+	protected static String testPassword(String password, String test) {
+		if(password.equals(test))
+			return "correct";
+		else
+			return "wrone";
 	}
 }
